b4dab13ab9d313c5fb5978f765986ae84409a2c6
Dan Actor <danltactor@gmail.com>
2017-11-15 18:32:38 -0700

    add js css method on all buttons

--- memory_match.js
+++ memory_match.js
@@ -1,37 +1,80 @@
 
 $('.option1').click( function() {
   $('.option1').css("display", "none")
-  $('.choice_back').css("display", "block");
+  $('.choice_back1').css("display", "block");
+});
+
+$('.option12').click( function() {
+  $('.option12').css("display", "none")
+  $('.choice_back12').css("display", "block");
 });
 
 $( ".option2" ).click(function() {
-  alert( "Option2" );
+  $('.option2').css("display", "none")
+  $('.choice_back2').css("display", "block");
+});
+
+$( ".option22" ).click(function() {
+  $('.option22').css("display", "none")
+  $('.choice_back22').css("display", "block");
 });
 
 $( ".option3" ).click(function() {
-  alert( "Option3" );
+  $('.option3').css("display", "none")
+  $('.choice_back3').css("display", "block");
+});
+
+$( ".option32" ).click(function() {
+  $('.option32').css("display", "none")
+  $('.choice_back32').css("display", "block");
 });
 
 $( ".option4" ).click(function() {
-  alert( "Option4" );
+  $('.option4').css("display", "none")
+  $('.choice_back4').css("display", "block");
+});
+
+$( ".option42" ).click(function() {
+  $('.option42').css("display", "none")
+  $('.choice_back42').css("display", "block");
 });
 
 $( ".option5" ).click(function() {
-  alert( "Option5" );
+  $('.option5').css("display", "none")
+  $('.choice_back5').css("display", "block");
+});
+
+$( ".option52" ).click(function() {
+  $('.option52').css("display", "none")
+  $('.choice_back52').css("display", "block");
 });
 
 $( ".option6" ).click(function() {
-  alert( "Option6" );
+  $('.option6').css("display", "none")
+  $('.choice_back6').css("display", "block");
+});
+
+$( ".option62" ).click(function() {
+  $('.option62').css("display", "none")
+  $('.choice_back62').css("display", "block");
 });
 
 $( ".option7" ).click(function() {
-  alert( "Option7" );
+  $('.option7').css("display", "none")
+  $('.choice_back7').css("display", "block");
+});
+
+$( ".option72" ).click(function() {
+  $('.option72').css("display", "none")
+  $('.choice_back72').css("display", "block");
 });
 
 $( ".option8" ).click(function() {
-  alert( "Option8" );
+  $('.option8').css("display", "none")
+  $('.choice_back8').css("display", "block");
 });
 
-// for (var i = 0; i < buttons.length; i++) {
-//   buttons[i].addEventListener('click', false);
-// }
+$( ".option82" ).click(function() {
+  $('.option82').css("display", "none")
+  $('.choice_back82').css("display", "block");
+});
