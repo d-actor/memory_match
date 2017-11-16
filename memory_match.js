19c70b83e873a536f9b2792bbc9cc66d007ae9c6
Dan Actor <danltactor@gmail.com>
2017-11-16 01:07:29 -0700

    clean up js

--- memory_match.js
+++ memory_match.js
@@ -1,165 +1,168 @@
+// Randomize the order of each cell on every page refresh
+// Instead of just numbers in the cells that you match, figure out how to use images or colors to match
+
 var userChoices = []
 var winCounter = []
 
-$('.option1').click( function() {
-  $('.option1').css("display", "none")
-  $('.choice_back1').css("display", "block");
+$('.btns_1').click( function() {
+  $('.option' + this.id).css("display", "none")
+  $('.choice_back' + this.id).css("display", "block");
+  userChoices.push(this.id)
+  if(userChoices.length === 2) {
+  evalChoices(userChoices)
+  }
+//   // console.log(userChoices)
+});
+//
+$('.btns_2').click( function() {
+  $('.option' + this.id + '2').css("display", "none")
+  $('.choice_back' + this.id + '2').css("display", "block");
   userChoices.push(this.id)
   if(userChoices.length === 2) {
   evalChoices(userChoices)
   }
   // console.log(userChoices)
 });
-
-$('.option12').click( function() {
-  $('.option12').css("display", "none")
-  $('.choice_back12').css("display", "block");
-  userChoices.push(this.id)
-  if(userChoices.length === 2) {
-  evalChoices(userChoices)
-  }
+//
+// $( ".option2" ).click(function() {
+//   $('.option2').css("display", "none")
+//   $('.choice_back2').css("display", "block");
+//   userChoices.push(this.id)
+//   if(userChoices.length === 2) {
+//   evalChoices(userChoices)
+//   }
+//   // console.log(userChoices)
+// });
+//
+// $( ".option22" ).click(function() {
+//   $('.option22').css("display", "none")
+//   $('.choice_back22').css("display", "block");
+//   userChoices.push(this.id)
+//   if(userChoices.length === 2) {
+//   evalChoices(userChoices)
+//   }
+//   // console.log(userChoices)
+// });
+//
+// $( ".option3" ).click(function() {
+//   $('.option3').css("display", "none")
+//   $('.choice_back3').css("display", "block");
+//   userChoices.push(this.id)
+//   if(userChoices.length === 2) {
+//   evalChoices(userChoices)
+//   }
+//   // console.log(userChoices)
+// });
+//
+// $( ".option32" ).click(function() {
+//   $('.option32').css("display", "none")
+//   $('.choice_back32').css("display", "block");
+//   userChoices.push(this.id)
+//   if(userChoices.length === 2) {
+//   evalChoices(userChoices)
+//   }
+//   // console.log(userChoices)
+// });
+//
+// $( ".option4" ).click(function() {
+//   $('.option4').css("display", "none")
+//   $('.choice_back4').css("display", "block");
+//   userChoices.push(this.id)
+//   if(userChoices.length === 2) {
+//   evalChoices(userChoices)
+//   }
+//   // console.log(userChoices)
+// });
+//
+// $( ".option42" ).click(function() {
+//   $('.option42').css("display", "none")
+//   $('.choice_back42').css("display", "block");
+//   userChoices.push(this.id)
+//   if(userChoices.length === 2) {
+//   evalChoices(userChoices)
+//   }
+//   // console.log(userChoices)
+// });
+//
+// $( ".option5" ).click(function() {
+//   $('.option5').css("display", "none")
+//   $('.choice_back5').css("display", "block");
+//   userChoices.push(this.id)
+//   if(userChoices.length === 2) {
+//   evalChoices(userChoices)
+//   }
+//   // console.log(userChoices)
+// });
+//
+// $( ".option52" ).click(function() {
+//   $('.option52').css("display", "none")
+//   $('.choice_back52').css("display", "block");
+//   userChoices.push(this.id)
+//   if(userChoices.length === 2) {
+//   evalChoices(userChoices)
+//   }
+//   // console.log(userChoices)
+// });
+//
+// $( ".option6" ).click(function() {
+//   $('.option6').css("display", "none")
+//   $('.choice_back6').css("display", "block");
+//   userChoices.push(this.id)
+//   if(userChoices.length === 2) {
+//   evalChoices(userChoices)
+//   }
+//   // console.log(userChoices)
+// });
+//
+// $( ".option62" ).click(function() {
+//   $('.option62').css("display", "none")
+//   $('.choice_back62').css("display", "block");
+//   userChoices.push(this.id)
+//   if(userChoices.length === 2) {
+//   evalChoices(userChoices)
+//   }
+//   // console.log(userChoices)
+// });
+//
+// $( ".option7" ).click(function() {
+//   $('.option7').css("display", "none")
+//   $('.choice_back7').css("display", "block");
+//   userChoices.push(this.id)
+//   if(userChoices.length === 2) {
+//   evalChoices(userChoices)
+//   }
+//   // console.log(userChoices)
+// });
+//
+// $( ".option72" ).click(function() {
+//   $('.option72').css("display", "none")
+//   $('.choice_back72').css("display", "block");
+//   userChoices.push(this.id)
+//   if(userChoices.length === 2) {
+//   evalChoices(userChoices)
+//   }
+//   // console.log(userChoices)
+// });
+//
+// $( ".option8" ).click(function() {
+//   $('.option8').css("display", "none")
+//   $('.choice_back8').css("display", "block");
+//   userChoices.push(this.id)
+//   if(userChoices.length === 2) {
+//   evalChoices(userChoices)
+//   }
+//   // console.log(userChoices)
+// });
+//
+// $( ".option82" ).click(function() {
+//   $('.option82').css("display", "none")
+//   $('.choice_back82').css("display", "block");
+//   userChoices.push(this.id)
+//   if(userChoices.length === 2) {
+//   evalChoices(userChoices)
+//   }
   // console.log(userChoices)
-});
-
-$( ".option2" ).click(function() {
-  $('.option2').css("display", "none")
-  $('.choice_back2').css("display", "block");
-  userChoices.push(this.id)
-  if(userChoices.length === 2) {
-  evalChoices(userChoices)
-  }
-  // console.log(userChoices)
-});
-
-$( ".option22" ).click(function() {
-  $('.option22').css("display", "none")
-  $('.choice_back22').css("display", "block");
-  userChoices.push(this.id)
-  if(userChoices.length === 2) {
-  evalChoices(userChoices)
-  }
-  // console.log(userChoices)
-});
-
-$( ".option3" ).click(function() {
-  $('.option3').css("display", "none")
-  $('.choice_back3').css("display", "block");
-  userChoices.push(this.id)
-  if(userChoices.length === 2) {
-  evalChoices(userChoices)
-  }
-  // console.log(userChoices)
-});
-
-$( ".option32" ).click(function() {
-  $('.option32').css("display", "none")
-  $('.choice_back32').css("display", "block");
-  userChoices.push(this.id)
-  if(userChoices.length === 2) {
-  evalChoices(userChoices)
-  }
-  // console.log(userChoices)
-});
-
-$( ".option4" ).click(function() {
-  $('.option4').css("display", "none")
-  $('.choice_back4').css("display", "block");
-  userChoices.push(this.id)
-  if(userChoices.length === 2) {
-  evalChoices(userChoices)
-  }
-  // console.log(userChoices)
-});
-
-$( ".option42" ).click(function() {
-  $('.option42').css("display", "none")
-  $('.choice_back42').css("display", "block");
-  userChoices.push(this.id)
-  if(userChoices.length === 2) {
-  evalChoices(userChoices)
-  }
-  // console.log(userChoices)
-});
-
-$( ".option5" ).click(function() {
-  $('.option5').css("display", "none")
-  $('.choice_back5').css("display", "block");
-  userChoices.push(this.id)
-  if(userChoices.length === 2) {
-  evalChoices(userChoices)
-  }
-  // console.log(userChoices)
-});
-
-$( ".option52" ).click(function() {
-  $('.option52').css("display", "none")
-  $('.choice_back52').css("display", "block");
-  userChoices.push(this.id)
-  if(userChoices.length === 2) {
-  evalChoices(userChoices)
-  }
-  // console.log(userChoices)
-});
-
-$( ".option6" ).click(function() {
-  $('.option6').css("display", "none")
-  $('.choice_back6').css("display", "block");
-  userChoices.push(this.id)
-  if(userChoices.length === 2) {
-  evalChoices(userChoices)
-  }
-  // console.log(userChoices)
-});
-
-$( ".option62" ).click(function() {
-  $('.option62').css("display", "none")
-  $('.choice_back62').css("display", "block");
-  userChoices.push(this.id)
-  if(userChoices.length === 2) {
-  evalChoices(userChoices)
-  }
-  // console.log(userChoices)
-});
-
-$( ".option7" ).click(function() {
-  $('.option7').css("display", "none")
-  $('.choice_back7').css("display", "block");
-  userChoices.push(this.id)
-  if(userChoices.length === 2) {
-  evalChoices(userChoices)
-  }
-  // console.log(userChoices)
-});
-
-$( ".option72" ).click(function() {
-  $('.option72').css("display", "none")
-  $('.choice_back72').css("display", "block");
-  userChoices.push(this.id)
-  if(userChoices.length === 2) {
-  evalChoices(userChoices)
-  }
-  // console.log(userChoices)
-});
-
-$( ".option8" ).click(function() {
-  $('.option8').css("display", "none")
-  $('.choice_back8').css("display", "block");
-  userChoices.push(this.id)
-  if(userChoices.length === 2) {
-  evalChoices(userChoices)
-  }
-  // console.log(userChoices)
-});
-
-$( ".option82" ).click(function() {
-  $('.option82').css("display", "none")
-  $('.choice_back82').css("display", "block");
-  userChoices.push(this.id)
-  if(userChoices.length === 2) {
-  evalChoices(userChoices)
-  }
-  // console.log(userChoices)
-});
+// });
 
 $( "#reset" ).click(function() {
   alert(".....Reset.....")
@@ -193,4 +196,4 @@ function evalChoices(userChoices) {
   alert("No luck my dude, try again or don't.")
   userChoices.length = 0
   }
-};
+}
